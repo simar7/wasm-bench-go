99bdbc522548638e4ca0bd1cbc95a7f52145a283
Oleh Herych <diesel.draft@gmail.com>
2022-10-17 20:43:11 +0300

    Updated versions of libraries (#1)
    * used up-to-date versions of libraries

--- wasm_bench_test.go
+++ wasm_bench_test.go
@@ -1,70 +1,48 @@
 package main
 
 import (
-	"bytes"
 	_ "embed"
 	"testing"
-
-	"github.com/bytecodealliance/wasmtime-go"
-	"github.com/wasmerio/wasmer-go/wasmer"
-
-	"github.com/mathetake/gasm/wasi"
-	gasm "github.com/mathetake/gasm/wasm"
 )
 
-//go:embed wasm/sum.wasm
-var sumTinyGoWasm string
-
-const wasmBytecode = `
-	(module
-	  (type (func (param i32 i32) (result i32)))
-	  (func (type 0)
-	    local.get 0
-	    local.get 1
-	    i32.add)
-	  (export "sum" (func 0)))
-`
+//go:embed testdata/tinygo/sum.wasm
+var sumTinyGoWasm []byte
 
 func BenchmarkWasmtime(b *testing.B) {
-	store := wasmtime.NewStore(wasmtime.NewEngine())
-	wasm, _ := wasmtime.Wat2Wasm(wasmBytecode)
+	fn, err := createWasmtime(sumTinyGoWasm)
+	if err != nil {
+		b.Fatal(err)
+	}
 
-	module, _ := wasmtime.NewModule(store.Engine, wasm)
-	instance, _ := wasmtime.NewInstance(store, module, []wasmtime.AsExtern{})
-
-	sum := instance.GetExport(store, "sum").Func()
 	b.ResetTimer()
 
 	for i := 0; i < b.N; i++ {
-		_, _ = sum.Call(store, 5, 37)
+		_ = fn()
 	}
 }
 
 func BenchmarkWasmer(b *testing.B) {
-	wasmBytes := []byte(wasmBytecode)
+	fn, err := createWasmer(sumTinyGoWasm)
+	if err != nil {
+		b.Fatal(err)
+	}
 
-	engine := wasmer.NewEngine()
-	store := wasmer.NewStore(engine)
-
-	module, _ := wasmer.NewModule(store, wasmBytes)
-
-	importObject := wasmer.NewImportObject()
-	instance, _ := wasmer.NewInstance(module, importObject)
-
-	sum, _ := instance.Exports.GetFunction("sum")
 	b.ResetTimer()
 
 	for i := 0; i < b.N; i++ {
-		_, _ = sum(5, 37)
+		_ = fn()
 	}
 }
 
-func BenchmarkGasm(b *testing.B) {
-	mod, _ := gasm.DecodeModule(bytes.NewBuffer([]byte(sumTinyGoWasm)))
-	vm, _ := gasm.NewVM(mod, wasi.New().Modules())
+func BenchmarkWazero(b *testing.B) {
+	fn, err := createWazero(sumTinyGoWasm)
+	if err != nil {
+		b.Fatal(err)
+	}
+
 	b.ResetTimer()
 
 	for i := 0; i < b.N; i++ {
-		_, _, _ = vm.ExecExportedFunction("sum", uint64(5), uint64(37))
+		_ = fn()
 	}
 }
